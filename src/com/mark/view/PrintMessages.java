4c18b2571e544b0921edd01e5456756ee9bf5a2d
markkulube <markkulube@users.noreply.github.com>
2020-04-22 03:22:07 -0400

    Added docstring for the methods.

--- src/com/mark/view/PrintMessages.java
+++ src/com/mark/view/PrintMessages.java
@@ -1,19 +1,44 @@
 package com.mark.view;
 
+/**
+ * A view class that prints various messages of the game to console.
+ * 
+ * @author Mark Kulube
+ * @email markkulube@gmail.com
+ *
+ */
 public class PrintMessages {
 	
-	public void welcome() {
+	/**
+	 * Welcome messages at game start.
+	 * 
+	 * @param maxGuesses
+	 */
+	public void welcome(int maxGuesses) {
 		System.out.println("***WELCOME TO HANGMANG***");
 		System.out.println("");
-		System.out.println("You are allowed 3 incorrect guesses");
+		System.out.println("You are allowed " + maxGuesses + " incorrect guesses");
 	}
 	
-	public void guessInstruction(int numGuesses, String currentGuess) {
-		System.out.println("Incorrect guesses = " + numGuesses + "/3");
+	/**
+	 * Prompt user to guess and submit an alphabet character.
+	 * 
+	 * @param numGuesses
+	 * @param maxGuesses
+	 * @param currentGuess
+	 */
+	public void guessInstruction(int numGuesses, int maxGuesses, String currentGuess) {
+		System.out.println("Incorrect guesses = " + numGuesses + "/" + maxGuesses);
 		System.out.println("Characters guessed: " + currentGuess);
 		System.out.println("Enter an alpha character to guess the word");
 	}
 	
+	/**
+	 * Feedback as to whether the guess was right or wrong.
+	 * 
+	 * @param charValidCorrect
+	 * @param userGuess
+	 */
 	public void correctGuessMessage(boolean charValidCorrect, String userGuess) {
 		if(charValidCorrect) {
 			System.out.println("Correct Guess - '"+userGuess+"' is in the secrect word");
@@ -23,20 +48,28 @@ public class PrintMessages {
 
 	}
 	
+	/** Delimiter after each guess. */
 	public void guessBoundary() {
 		System.out.println("\n##################################################\n");
 	}
 	
+	/**
+	 * End game message if the user correctly guesses all characters
+	 * 
+	 * @param currentGuess
+	 */
 	public void winMessage(String currentGuess) {
 		System.out.println("****You Win****");
 		System.out.println("The Secret Word Is: "+currentGuess);
 		System.out.println("****Game Over****");
 	}
 	
-	public void loseMessage(int numGuesses) {
+	/** End game message if the user runs out of guess attempts. */
+	public void loseMessage(String secretWord, int numGuesses, int maxGuesses) {
 		System.out.println("****You Lose****");
-		System.out.println("Too Many Incorrect Guesses: "+ numGuesses+ "/3");
-		System.out.println("Try Again");
+		System.out.println("Too Many Incorrect Guesses: "+ numGuesses+ "/" + maxGuesses);
+		System.out.println("The Secret Word Was: " + secretWord);
+		System.out.println("Try Again!!!");
 		System.out.println("****Game Over****");
 	}
 
